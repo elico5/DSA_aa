eeebbb52c864a52b7abbda6dc9d845769cb1cb16
ecohen <elicohen763@gmail.com>
2019-09-18 14:43:21 -0400

    Code simple queue

---
+++ queue_project/lib/queue.js
@@ -0,0 +1,71 @@
+// ============================================================================
+// Implementation Exercise: Queue
+// ============================================================================
+//
+// -------
+// Prompt:
+// -------
+//
+// Implement a Queue and all of its methods below!
+//
+// ------------
+// Constraints:
+// ------------
+//
+// Make sure the time and space complexity of each is equivalent to those 
+// in the table provided in the Time and Space Complexity Analysis section
+// of your Stack reading!
+//
+// -----------
+// Let's Code!
+// -----------
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.front = null;
+        this.back = null;
+        this.length = 0;
+    }
+
+    enqueue(value) {
+        const newNode = new Node(value);
+        if (!this.length) {
+            [this.front, this.back] = [newNode, newNode];
+        } else {
+            this.back.next = newNode;
+            this.back = newNode;
+        }
+        this.length++;
+        return this.length;
+    }
+
+    dequeue() {
+        if (!this.length) {
+            return null;
+        } else {
+            const returnValue = this.front.value;
+            if (this.length === 1) {
+                this.front = null;
+                this.back = null;
+            } else {
+                this.front = this.front.next;
+            }
+            this.length--;
+            return returnValue;
+        }
+    }
+
+    size() {
+        return this.length;
+    }
+}
+
+exports.Node = Node;
+exports.Queue = Queue;
