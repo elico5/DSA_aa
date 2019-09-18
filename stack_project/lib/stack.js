c23d3cfded607ddfea97600ff3e84a7dfcef9953
ecohen <elicohen763@gmail.com>
2019-09-18 14:16:58 -0400

    Code basic stack

--- stack_project/lib/stack.js
+++ stack_project/lib/stack.js
@@ -21,11 +21,50 @@
 // -----------
 
 class Node {
-
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
 }
 
 class Stack {
+    constructor() {
+        this.top = null;
+        this.bottom = null;
+        this.length = 0;
+    }
 
+    push(value) {
+        const newNode = new Node(value);
+        if (!this.length) {
+            [this.top, this.bottom] = [newNode, newNode];
+        } else {
+            newNode.next = this.top;
+            this.top = newNode;
+        }
+        this.length++;
+        return this.length;
+    }
+
+    pop() {
+        if (!this.length) {
+            return null;
+        } else {
+            const returnValue = this.top.value;
+            if (this.length === 1) {
+                this.top = null;
+                this.bottom = null;
+            } else {
+                this.top = this.top.next;
+            }
+            this.length--;
+            return returnValue;
+        }
+    }
+
+    size() {
+        return this.length;
+    }
 }
 
 exports.Node = Node;
